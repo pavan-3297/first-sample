21a7d1051998c33fe7ab3442a673f401732ff9a2
Yogesh <yogeshrajput335@gmail.com>
2025-03-03 15:40:03 +0530

    added Some Change on TableRender

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import TableRender from './TableRender';
+import Example from './Example';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+   {/* <TableRender/> */}
+   <login-signup/>
+    {/* <Example/> */}
   </React.StrictMode>
 );
 
